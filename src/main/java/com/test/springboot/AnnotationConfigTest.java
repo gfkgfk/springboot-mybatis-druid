cc423598a59f43cfec9ca10d8b9a998797d0b4b6
gfkgfk <gfk000@126.com>
2022-08-17 17:43:19 +0800

    add auto config

--- src/main/java/com/test/springboot/AnnotationConfigTest.java
+++ src/main/java/com/test/springboot/AnnotationConfigTest.java
@@ -1,13 +1,18 @@
 package com.test.springboot;
 
-import com.test.springboot.annotation.enable.EnableAnnotation;
-import com.test.springboot.annotation.enable.EnableAnnotation2interface;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@EnableAutoConfiguration //@ComponentScan 注解只能扫描 spring-boot 项目包内的 bean 并注册到 spring 容器中，因此需要 @EnableAutoConfiguration 注解来注册项目包外的bean。而 spring.factories 文件，则是用来记录项目包外需要注册的bean类名。
+@EnableAutoConfiguration(exclude = {
+        DruidDataSourceAutoConfigure.class,
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,}) //需要exclude掉，否则会冲突
+//@ComponentScan 注解只能扫描 spring-boot 项目包内的 bean 并注册到 spring 容器中，因此需要 @EnableAutoConfiguration 注解来注册项目包外的bean。而 spring.factories 文件，则是用来记录项目包外需要注册的bean类名。
 public class AnnotationConfigTest {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = new SpringApplicationBuilder(AnnotationConfigTest.class)
