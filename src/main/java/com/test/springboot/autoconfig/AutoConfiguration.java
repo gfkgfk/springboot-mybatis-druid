23b410830604a90391591b36ece10661f8bdb6da
gfkgfk <gfk000@126.com>
2022-08-17 17:24:14 +0800

    update

--- src/main/java/com/test/springboot/autoconfig/AutoConfiguration.java
+++ src/main/java/com/test/springboot/autoconfig/AutoConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAnnotation2interface
-@ConditionalOnProperty(name = "helloword",havingValue = "true") //通过与配置文件中对应的属性的值进行比较，如果一致则进行注入，否则不进行注入
+@ConditionalOnProperty(name = "helloworld",havingValue = "true") //通过与配置文件中对应的属性的值进行比较，如果一致则进行注入，否则不进行注入
 public class AutoConfiguration {
 
 
